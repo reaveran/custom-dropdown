158ab572f86fb00eeda2625e68f8bcd12ef2b063
reaveran <reginaavenanaomi@gmail.com>
2024-07-28 12:55:43 +0700

    package configuration

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,3 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import Dropdown from "./components/Dropdown/Dropdown";
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+export { Dropdown };
